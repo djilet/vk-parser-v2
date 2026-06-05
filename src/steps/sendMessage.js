8c71975e8bbeeafdd1ee21943b4c199bce25e52a
Voytenko Anton <anton147147147@mail.ru>
2026-06-05 14:49:53 +0700

    remove unused data

--- src/steps/sendMessage.js
+++ src/steps/sendMessage.js
@@ -60,8 +60,8 @@ export async function sendCommunityMessage(page, messageText) {
   console.log(`Текст вставлен, курсор в конце. Жду ${BEFORE_SEND_MS / 1000}с перед отправкой...`);
   await sleep(BEFORE_SEND_MS);
 
-  // const sendButton = await page.waitForSelector(SEND_BUTTON_SELECTOR, { timeout: 15_000 });
-  // await sendButton.click();
+  const sendButton = await page.waitForSelector(SEND_BUTTON_SELECTOR, { timeout: 15_000 });
+  await sendButton.click();
 
   console.log('Сообщение отправлено.');
 }

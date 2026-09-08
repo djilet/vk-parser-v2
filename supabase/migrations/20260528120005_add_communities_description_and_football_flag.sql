97a4b6b57ec18bf4e34aa639cc3ab1021770e3c5
Voytenko Anton <anton147147147@mail.ru>
2026-09-08 12:19:26 +0700

    Fixes

--- supabase/migrations/20260528120005_add_communities_description_and_football_flag.sql
+++
@@ -1,3 +0,0 @@
-alter table public.communities
-  add column description text,
-  add column is_football_tournament boolean not null default false;
---
+++ supabase/migrations/20260908120002_add_communities_description.sql
@@ -0,0 +1,2 @@
+alter table public.communities
+  add column description text not null default '';
